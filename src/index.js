f8d48bc08683a95a50c2390aceeffb68858badc4
DanteBoom <salamandrboom@gmail.com>
2018-09-30 20:18:27 +0300

    additional_5: completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let input = str.split('');
+  let countTypes = bracketsConfig.length;
+  let closingBracket;
+  let count = input.length;
+  
+  for(let indexOpen = 0; indexOpen < count; indexOpen++){
+    if(input[indexOpen]){
+      closingBracket = getClosingBracket(input[indexOpen]);
+      let tagFound = false;
+      for(let indexClose = indexOpen + 1; indexClose < count; indexClose += 2){
+        if(input[indexClose] == closingBracket){          
+          delete input[indexClose];
+          delete input[indexOpen];
+          tagFound = true;
+          break;
+        }
+      }
+      if(tagFound == false)
+        return false;
+    }
+  }
+  return true;
+
+  function getClosingBracket (openBracket) {
+    if(countTypes == 1){
+      return bracketsConfig[0][1];
+    }else{
+      for(let arrI = 0; arrI < countTypes; arrI++){
+        if(bracketsConfig[arrI][0] == openBracket)
+        return bracketsConfig[arrI][1];
+      }
+    }
+  }
 }
